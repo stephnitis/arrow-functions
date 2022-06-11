de12c38cbe346a00ad64f6dd634f962b2ef41d50
Stephanie Hill <stephni.hill@gmail.com>
2022-06-11 14:42:16 -0700

    fin

--- starter-code/app.js
+++ starter-code/app.js
@@ -140,16 +140,16 @@ console.log(sumAndProduct(3, 9));
 let message = name => `Hello, ${name}!`;
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-console.log(message('Allie'));
+// console.log(message('Allie'));
 
 
-const newStudent = studentArray => ({
-  name : studentArray[0],
-  age : studentArray[1],
-  hometown : studentArray[2],
-});
+let Student = function(name, age, hometown) {
+  this.name = name;
+  this.age = age;
+  this.hometown = hometown;
+};
 
-let joe = newStudent(['Joe', 'Schmoe', 100]);
+let joe = new Student('Joe', 'Schmoe', 100);
 
 // TODO: Uncomment the following line of code to see the output in the browser console
 // Note that the arrow function will cause this code to break!
@@ -159,23 +159,22 @@ console.log(joe);
 
 
 
-let greeting = `Hi, my name is ${joe.name}`;
-
+Student.prototype.greeting = function() {
+  return `Hi, my name is ${this.name}`;
+};
 
 // TODO: Uncomment the following line of code to see the output in the browser console
 // Note that the arrow function will cause this method to break!
-console.log(joe.greeting);
+console.log(joe.greeting());
 
 // TODO: After viewing the previous console.log(), return the code to a working state.
 
 
 
-Student.courseName = function() {
-  return 'This student is enrolled in Code 301.';
-};
+Student.courseName = () => {return 'This student is enrolled in Code 301.'};
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// console.log(Student.courseName());
+console.log(Student.courseName());
 
 
 
@@ -186,17 +185,17 @@ Student.prototype.scope = function() {
 };
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// joe.scope();
+joe.scope();
 
 Student.prototype.scopeArrow = () => console.log(this);
 
 // TODO: Uncomment the following line of code to see the output in the browser console
-// joe.scopeArrow();
+joe.scopeArrow();
 
 // TODO: Write a COMMENT below to answer the following questions.
 // 1. What is "this" when joe.scope() is invoked?
-//
+// It refers to the object constuctor and the properties belonging to the object - in this case the "Student" constructor
 // 2. What is "this" when joe.scopeArrow() is invoked?
-//
+// This is set to the global object in this case the window/browser object.
 // 3. Explain why "this" is different when an arrow function is used.
-//
+// Because arrow functions establish "this" based on the scope the arrow function is defined within. Thus "this" retains the value of the enclosing lexical context's "this" and in global code it will be set to the global object.
